01c26b29a43305163ef968ab23ab6fa127cd048e
unknown <clm7a5@yahoo.com>
2020-09-21 22:53:17 -0500

    1-5 Requirements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,4 +13,26 @@ export class AppComponent {
   sourceList: Satellite[];
 
 //set sourceList to be an array of Satellite objects
+//delete 1st constructor and replace it
+constructor() {
+  this.sourceList = [];
+  let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+
+  window.fetch(satellitesUrl).then(function(response) {
+     response.json().then(function(data) {
+
+        let fetchedSatellites = data.satellites;
+        // TODO: loop over satellites
+        for(let i = 0; i < fetchedSatellites.length; i++) {
+        // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+        let satelliteObject = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+        // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
+        this.sourceList.push(satelliteObject);
+        }
+        
+
+     }.bind(this));
+  }.bind(this));
+
+}
 }

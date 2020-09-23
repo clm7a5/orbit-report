ccab191c559b952f077afbdf6d16a8a22246e13d
unknown <clm7a5@yahoo.com>
2020-09-23 16:29:35 -0500

    Finished Sorting Requirement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 //6b add to top
-import { Satellite } from './satellite';
 import { Component } from '@angular/core';
+import { Satellite } from './satellite';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +11,13 @@ export class AppComponent {
   title = 'orbit-report';
   //define sourceList
   sourceList: Satellite[];
+  displayList: Satellite[];
+
 
 //set sourceList to be an array of Satellite objects
 //delete 1st constructor and replace it
 constructor() {
+  this.displayList = []
   this.sourceList = [];
   let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
 
@@ -23,11 +26,11 @@ constructor() {
 
         let fetchedSatellites = data.satellites;
         // TODO: loop over satellites
-        for(let i = 0; i < fetchedSatellites.length; i++) {
+        for (let i = 0; i < fetchedSatellites.length; i++) {
         // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
-        let satelliteObject = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+        let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
         // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
-        this.sourceList.push(satelliteObject);
+        this.sourceList.push(satellite);
         }
         
 
